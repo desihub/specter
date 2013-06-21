c3f44a2be0e94416755c785b3bb2bff6a0dd23ef
Stephen Bailey <stephen.code@gmail.com>
2013-06-21 12:07:43 -0700

    2d extractions work with tests

--- py/extract/ex2d.py
+++ py/extract/ex2d.py
@@ -4,6 +4,73 @@
 
 import numpy as N
 from scipy.sparse import spdiags, issparse
+from scipy.sparse.linalg import spsolve
+
+def ex2d(image, ivar, psf, specrange, wavelengths, full_output=False):
+    """
+    2D PSF extraction of flux from image given pixel inverse variance.
+    
+    Inputs:
+        image : 2D array of pixels
+        ivar  : 2D array of inverse variance for the image
+        psf   : PSF object
+        specrange : (specmin, specmax) inclusive to extract
+        wavelengths : 1D array of wavelengths to extract
+        
+    Returns (flux, ivar, R):
+        flux[nspec, nwave] = extracted resolution convolved flux
+        ivar[nspec, nwave] = inverse variance of flux
+        R : 2D resolution matrix to convert
+        
+    if full_output is True, return dictionary of the above plus additional
+    intermediate outputs such as the projection matrix.
+        
+    To do: Add ability to input subimage instead of entire image
+    """
+    
+    #- Range of image to consider
+    waverange = (wavelengths[0], wavelengths[-1])
+    xmin, xmax, ymin, ymax = xyrange = psf.xyrange(specrange, waverange)
+    nx, ny = xmax-xmin, ymax-ymin
+    npix = nx*ny
+    image = image[ymin:ymax, xmin:xmax]
+    ivar = ivar[ymin:ymax, xmin:xmax]
+    
+    nspec = specrange[1] - specrange[0]
+    nflux = len(wavelengths)
+    
+    #- Pixel weights matrix
+    W = spdiags(ivar.ravel(), 0, npix, npix)
+    
+    #- Projection matrix and inverse covariance
+    A = psf.projection_matrix(specrange, wavelengths, xyrange)
+    iCov = A.T.dot(W.dot(A))
+    
+    #- Solve image = A flux weighted by W:
+    #-     A^T W image = (A^T W A) flux = iCov flux    
+    y = A.T.dot(W.dot(image.ravel()))
+    
+    xflux = spsolve(iCov, y).reshape((nspec, nflux))
+
+    #- Convolve with Resolution matrix to decorrelate errors
+    R = resolution_from_icov(iCov)
+    rflux = R.dot(xflux.ravel()).reshape(xflux.shape)
+
+    #- Note: this could be faster by using calculations already done in
+    #- resolution_from_iCov()
+    Cov = N.linalg.inv(iCov.toarray())
+    rCov = R.T.dot(Cov.dot(R))
+    ivar = 1.0/rCov.diagonal().reshape(rflux.shape)
+
+    if full_output:
+        results = dict(flux=rflux, ivar=ivar, R=R, xflux=xflux, projmat=A)
+        results['Cov'] = Cov
+        results['iCov'] = iCov
+        results['rCov'] = rCov
+        return results
+    else:
+        return rflux, ivar, R
+    
 
 def sym_sqrt(a):
     """
