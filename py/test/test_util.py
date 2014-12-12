956064e9177833a325a2572c3569cddbbb381754
Stephen Bailey <StephenBailey@lbl.gov>
2014-12-12 11:15:41 -0800

    fixed sincshift bug in y; added test

--- py/test/test_util.py
+++ py/test/test_util.py
@@ -6,7 +6,7 @@ Unit tests for PSF classes.
 
 import sys
 import os
-import numpy as N
+import numpy as np
 from numpy.polynomial import legendre
 from specter import util
 import unittest
@@ -30,40 +30,49 @@ class TestUtil(unittest.TestCase):
                 self.assertTrue(util.gaussint(-x, sigma=x) == 0.15865525393145707)
 
     def test_trapz(self):
-        x = N.linspace(0, 2, 20)
-        y = N.sin(x)
+        x = np.linspace(0, 2, 20)
+        y = np.sin(x)
         #- Check integration over full range
         edges = (x[0], x[-1])
-        self.assertTrue(N.trapz(y, x) == util.trapz(edges, x, y)[0])
+        self.assertTrue(np.trapz(y, x) == util.trapz(edges, x, y)[0])
         
         #- Check that integrations add up to each other
         lo = 0.5*(x[0] + x[1])
         hi = 0.5*(x[-2] + x[-1])
         mid = 0.5*(lo + hi)
-        edges = N.linspace(lo, hi, 30)
+        edges = np.linspace(lo, hi, 30)
         blat = util.trapz(edges, x, y)
         foo = util.trapz([lo, hi], x, y)
         bar = util.trapz([lo, mid, hi], x, y)
-        self.assertTrue(N.sum(blat) == foo[0])
-        self.assertTrue(N.sum(blat) == N.sum(bar))
+        self.assertTrue(np.sum(blat) == foo[0])
+        self.assertTrue(np.sum(blat) == np.sum(bar))
         
         #- Edges outside of range shouldn't crash
         blat = util.trapz([-1,0,1,2], x, y)
         
+    def test_sincshift(self):
+        a = np.zeros((3,5))
+        self.assertTrue(a.shape == util.sincshift(a, 0.1, 0.0).shape)
+        self.assertTrue(a.shape == util.sincshift(a, 0.0, 0.1).shape)
+        self.assertTrue(a.shape == util.sincshift(a, 0.1, 0.1).shape)
+        self.assertTrue(a.shape == util.sincshift2d(a, 0.1, 0.0).shape)
+        self.assertTrue(a.shape == util.sincshift2d(a, 0.0, 0.1).shape)
+        self.assertTrue(a.shape == util.sincshift2d(a, 0.1, 0.1).shape)
+        
     # def test_rebin(self):
-    #     x = N.arange(25)
-    #     y = N.random.uniform(0.0, 5.0, size=len(x))
+    #     x = np.arange(25)
+    #     y = np.random.uniform(0.0, 5.0, size=len(x))
     #     y[0] = y[-1] = 0.0  #- avoid edge bin effects for comparison
-    #     xx = N.arange(0, len(x), 1.3)
+    #     xx = np.arange(0, len(x), 1.3)
     #     yy = util.rebin(xx, x, y)
     #     
-    #     s1 = N.sum(y * N.gradient(x))
-    #     s2 = N.sum(yy * N.gradient(xx))
+    #     s1 = np.sum(y * np.gradient(x))
+    #     s2 = np.sum(yy * np.gradient(xx))
     #     self.assertAlmostEqual(s1, s2)
     #     
-    #     edges = N.linspace(0, len(x))
+    #     edges = np.linspace(0, len(x))
     #     zz = util.rebin(edges, x, y, edges=True)
-    #     s3 = N.sum(yy * N.gradient(xx))
+    #     s3 = np.sum(yy * np.gradient(xx))
     #     self.assertEqual(len(zz), len(edges)-1)
     #     self.assertAlmostEqual(s1, s3)
     #     
