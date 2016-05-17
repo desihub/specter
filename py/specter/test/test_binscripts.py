01491a8b49bb978a30fc404b9ba27db5d925a6bd
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-05-17 18:33:50 -0400

    new path to sky-uves file

--- py/specter/test/test_binscripts.py
+++ py/specter/test/test_binscripts.py
@@ -10,7 +10,7 @@ import numpy as np
 import unittest
 from astropy.io import fits
 from uuid import uuid4
-
+from pkg_resources import resource_filename
 from specter.test import test_data_dir
 import specter.io
 
@@ -32,6 +32,7 @@ class TestBinScripts(unittest.TestCase):
                     )
                 )
             )
+        self.sky_file = resource_filename('specter', 'data/sky-uves.fits')
         self.exspec_cmd = """{executable} {specter_dir}/bin/exspec \
           -i {imgfile} \
           -p {specter_dir}/data/test/psf-monospot.fits \
@@ -46,7 +47,7 @@ class TestBinScripts(unittest.TestCase):
         except KeyError:
             self.origPath = None
             os.environ['PYTHONPATH'] = os.path.join(self.specter_dir,'py')
-        
+
     def tearDown(self):
         if self.origPath is None:
             del os.environ['PYTHONPATH']
@@ -55,7 +56,7 @@ class TestBinScripts(unittest.TestCase):
 
     def test_aa(self):
         cmd = """{executable} {specter_dir}/bin/specter \
-          -i {specter_dir}/data/sky/sky-uves.fits \
+          -i {sky} \
           -p {specter_dir}/data/test/psf-monospot.fits \
           -t {specter_dir}/data/test/throughput.fits \
           -o {imgfile} \
@@ -63,22 +64,23 @@ class TestBinScripts(unittest.TestCase):
           -n --specmin 0 --nspec 2 --exptime 1500""".format(
             executable=sys.executable,
             specter_dir=self.specter_dir,
+            sky=self.sky_file,
             imgfile = imgfile1,
             )
         print(cmd)
         err = os.system(cmd)
         self.assertEqual(err, 0, 'Error code {} != 0'.format(err))
         self.assertTrue(os.path.exists(imgfile1))
-        
+
         fx = fits.open(imgfile1)
         self.assertTrue('CCDIMAGE' in fx)
         self.assertTrue('IVAR' in fx)
         fx.close
-        
+
         #- Test the I/O routines while we have the file handy
         image, ivar, hdr = specter.io.read_image(imgfile1)
         self.assertTrue(image.shape == ivar.shape)
-        
+
         os.remove(imgfile1)
         cmd = cmd + ' --extra'
         err = os.system(cmd)
@@ -102,22 +104,22 @@ class TestBinScripts(unittest.TestCase):
             err = os.system(cmd)
             self.assertEqual(err, 0, 'Error code {} != 0 with dwave={}'.format(err, dwave))
             self.assertTrue(os.path.exists(specfile1))
-            
+
         fx = fits.open(specfile1)
         print(fx.info())
         self.assertTrue('FLUX' in fx)
         self.assertTrue('IVAR' in fx)
         self.assertTrue('WAVELENGTH' in fx)
         self.assertTrue('RESOLUTION' in fx)
-        
+
         #- this is covered in the exspec binscript, but not yet visible to
         #- coverage tools; try it here just for good measure
         specter.io.write_spectra(specfile2,
             fx['WAVELENGTH'].data, fx['FLUX'].data,
             fx['IVAR'].data, fx['RESOLUTION'].data, fx[0].header)
-        
+
         fx.close()
-            
+
 
     def test_cc(self):
         #- Also check it works for the last fibers and not just the first ones
@@ -136,19 +138,20 @@ class TestBinScripts(unittest.TestCase):
     def test_dd(self):
         """Test both single core and dual core running"""
         cmd = """{executable} {specter_dir}/bin/specter \
-          -i {specter_dir}/data/sky/sky-uves.fits \
+          -i {sky} \
           -p {specter_dir}/data/test/psf-monospot.fits \
           -t {specter_dir}/data/test/throughput.fits \
           -w 7500,7620 \
           --specmin 0 --nspec 2 --exptime 1500 --trimxy""".format(
               executable=sys.executable,
+              sky=self.sky_file,
               specter_dir=self.specter_dir)
 
         if os.path.exists(imgfile1):
             os.remove(imgfile1)
         if os.path.exists(imgfile2):
             os.remove(imgfile2)
-        
+
         err = os.system(cmd + " --numcores 1 -o " + imgfile1)
         self.assertEqual(err, 0, 'Error code {} != 0'.format(err))
         self.assertTrue(os.path.exists(imgfile1))
@@ -156,10 +159,10 @@ class TestBinScripts(unittest.TestCase):
         err = os.system(cmd + " --numcores 2 -o " + imgfile2)
         self.assertEqual(err, 0, 'Error code {} != 0'.format(err))
         self.assertTrue(os.path.exists(imgfile2))
-        
+
         img1 = fits.getdata(imgfile1)
         img2 = fits.getdata(imgfile2)
-        
+
         self.assertTrue(np.allclose(img1, img2))
 
     @classmethod
