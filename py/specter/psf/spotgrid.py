fa547c7a60fd7ae271b34736231352084fc89ded
Laurie Stephey <lastephey@github.com>
2017-11-09 15:53:35 -0800

    in python the command is shape not size

--- py/specter/psf/spotgrid.py
+++ py/specter/psf/spotgrid.py
@@ -124,10 +124,10 @@ class SpotGridPSF(PSF):
         resampled_pix_spot_values[dy:ny_spot+dy,dx+1:nx_spot+dx+1]     += w01*pix_spot_values
         resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx+1:nx_spot+dx+1] += w11*pix_spot_values
         #print statements to see array sizes
-        print(resampled_pix_spot_values[dy:ny_spot+dy,dx:nx_spot+dx].size)
-        print(resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx:nx_spot+dx].size)
-        print(resampled_pix_spot_values[dy:ny_spot+dy,dx+1:nx_spot+dx+1].size)
-        print(resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx+1:nx_spot+dx+1].size)
+        print(resampled_pix_spot_values[dy:ny_spot+dy,dx:nx_spot+dx].shape)
+        print(resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx:nx_spot+dx].shape)
+        print(resampled_pix_spot_values[dy:ny_spot+dy,dx+1:nx_spot+dx+1].shape)
+        print(resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx+1:nx_spot+dx+1].shape)
 
         
         resample_t1=time.time()
