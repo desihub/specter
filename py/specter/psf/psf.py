8d3209257d20a622a8609af2c0f05ba25ebc410a
Laurie Stephey <lastephey@github.com>
2017-11-10 11:09:55 -0800

    re-enable _xypix1 timer since we are reaching it

--- py/specter/psf/psf.py
+++ py/specter/psf/psf.py
@@ -342,7 +342,7 @@ class PSF(object):
         wavelength_frac=wavelength_elapsed_t/xypix_elapsed_t
         lohi_frac=lohi_elapsed_t/xypix_elapsed_t
         #cache_frac=cache_elapsed_t/xypix_elapsed_t
-        #_xypix1_frac=_xypix1_elapsed_t/xypix_elapsed_t
+        _xypix1_frac=_xypix1_elapsed_t/xypix_elapsed_t
         #_xypix2_frac=_xypix2_elapsed_t/xypix_elapsed_t
         lohi_start_stop_frac=lohi_start_stop_elapsed_t/xypix_elapsed_t
         edge_check_frac=edge_check_elapsed_t/xypix_elapsed_t
@@ -351,7 +351,7 @@ class PSF(object):
         print("xypix wavelength fraction used is %s" %(wavelength_frac))
         print("xypix lohi fraction used is %s" %(lohi_frac))
         #print("xypix cache fraction used is %s" %(cache_frac))
-        #print("xypix _xypix1 fraction used is %s" %(_xypix1_frac))
+        print("xypix _xypix1 fraction used is %s" %(_xypix1_frac))
         #print("xypix _xypix2 fraction used is %s" %(_xypix2_frac))
         print("xypix lohi_start_stop fraction used is %s" %(lohi_start_stop_frac))
         print("xypix edge_check fraction used is %s" %(edge_check_frac))
