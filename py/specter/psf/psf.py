f5b01bd46406c8e0c63d7cf5f29e1fcd0ec6b711
Laurie Stephey <lastephey@github.com>
2017-11-10 10:46:58 -0800

    try again to put timers in right place

--- py/specter/psf/psf.py
+++ py/specter/psf/psf.py
@@ -258,11 +258,8 @@ class PSF(object):
                 #timing for _xypix1 ---------------------------------------
                 _xypix1_t0=time.time()
                 xx, yy, ccdpix = self._xypix(ispec, wavelength)
-                _xypix1_t1=time.time()
+                _xypix1_t1=time.time() # ---------------------------------
                 self._cache[key] = (xx, yy, ccdpix)
-                
-        #done timing for _xypix1 ----------------------------------
-        _xypix1_elapsed_t=_xypix1_t1-xypix1_t0
         
                 
         except AttributeError:
@@ -270,9 +267,12 @@ class PSF(object):
             #timing for _xypix2 ---------------------------------------
             _xypix2_t0=time.time()        
             xx, yy, ccdpix = self._xypix(ispec, wavelength)
-            _xypix2_t1=time.time()
+            _xypix2_t1=time.time() # -----------------------------------
             
-        #done timing for _xypix2 ---------------------------------
+        #done timing for _xypix1 
+        _xypix1_elapsed_t=_xypix1_t1-xypix1_t0    
+            
+        #done timing for _xypix2 
         _xypix2_elapsed_t=_xypix2_t1-_xypix2_t0        
             
         lohi_t1=time.time()
