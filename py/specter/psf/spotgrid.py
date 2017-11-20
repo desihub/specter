da5cb5503cca10db503c51fc0929585db34197a6
Laurie <lastephey@lbl.gov>
2017-11-20 10:08:06 -0800

    try reverting back to what ran last night

--- py/specter/psf/spotgrid.py
+++ py/specter/psf/spotgrid.py
@@ -110,10 +110,10 @@ class SpotGridPSF(PSF):
         pix_spot_list=pix_spot_values.tolist()
         #print(len(pix_spot_list),len(pix_spot_list[0]))
         resampled_pix_spot_list=[[0 for x in range (ny_spot + rebin)] for y in range(nx_spot+rebin)]
-        pix_prod_bl = [[x * w00 for line in pix_spot_list] for x in line] #bottom left
-        pix_prod_br = [[x * w10 for line in pix_spot_list] for x in line] #bottom right
-        pix_prod_tl = [[x * w01 for line in pix_spot_list] for x in line] #top left
-        pix_prod_tr = [[x * w11 for line in pix_spot_list] for x in line] #top right
+        pix_prod_bl = [[x * w00 for a in pix_spot_list] for x in pix_spot_list[0]] #bottom left
+        pix_prod_br = [[x * w10 for a in pix_spot_list] for x in pix_spot_list[0]] #bottom right
+        pix_prod_tl = [[x * w01 for a in pix_spot_list] for x in pix_spot_list[0]] #top left
+        pix_prod_tr = [[x * w11 for a in pix_spot_list] for x in pix_spot_list[0]] #top right
         
         #print(pix_spot_list), this is okay, has values
         #pix_prod_bl does not have values, is empty
