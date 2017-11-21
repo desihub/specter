a214990a34a6680f3a5555f2eccfc5f59ba1e019
Laurie <lastephey@lbl.gov>
2017-11-20 18:14:49 -0800

    okay gave it 5 types

--- py/specter/util/util.py
+++ py/specter/util/util.py
@@ -74,7 +74,7 @@ class LinearInterp2D(object):
         #jit doesn't like passing in the self argument
         data_temp=self.data
 
-        @vectorize(["float32(float32)", "float64(float64)"])            
+        @vectorize(["float64(float64)", "float64(float64)", "float64(float64)", "float64(float64)", "float64(float64)"])            
         
         def _interp(data_temp,dx,dy,ix,iy):
             """
