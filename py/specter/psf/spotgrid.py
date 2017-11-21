8df267d388822500cf2c55e448d25afc8b43afb2
Laurie <lastephey@lbl.gov>
2017-11-20 16:55:44 -0800

    deleted too much

--- py/specter/psf/spotgrid.py
+++ py/specter/psf/spotgrid.py
@@ -101,6 +101,8 @@ class SpotGridPSF(PSF):
             resampled_pix_spot_values_temp[dy+1:ny_spot+dy+1,dx:nx_spot+dx]     += w10*pix_spot_values
             resampled_pix_spot_values_temp[dy:ny_spot+dy,dx+1:nx_spot+dx+1]     += w01*pix_spot_values
             resampled_pix_spot_values_temp[dy+1:ny_spot+dy+1,dx+1:nx_spot+dx+1] += w11*pix_spot_values
+            
+            return resampled_pix_spot_values_temp
 
             
         #have to actually call our subfunction!
