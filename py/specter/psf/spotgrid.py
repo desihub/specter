682b260d13d141aa20dc7540c09e1081028ff614
Laurie <lastephey@lbl.gov>
2017-11-29 11:26:50 -0800

    move itt around whole xypix function

--- py/specter/psf/spotgrid.py
+++ py/specter/psf/spotgrid.py
@@ -64,7 +64,8 @@ class SpotGridPSF(PSF):
         Return xslice, yslice, pix for PSF at spectrum ispec, wavelength
         """
         
-
+        # add itt tag
+        itt.resume()  
         
         #- Ratio of CCD to Spot pixel sizes
         rebin = int(self.CcdPixelSize / self.SpotPixelSize)
@@ -93,12 +94,11 @@ class SpotGridPSF(PSF):
         # resampled spot grid
         resampled_pix_spot_values=np.zeros((ny_spot+rebin,nx_spot+rebin))   
         
-        # add itt tag
-        itt.resume()         
+       
 
         resampled_pix_spot_values[dy:ny_spot+dy,dx:nx_spot+dx]         += w00*pix_spot_values
         
-        itt.detach()
+        
         resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx:nx_spot+dx]     += w10*pix_spot_values
         resampled_pix_spot_values[dy:ny_spot+dy,dx+1:nx_spot+dx+1]     += w01*pix_spot_values
         resampled_pix_spot_values[dy+1:ny_spot+dy+1,dx+1:nx_spot+dx+1] += w11*pix_spot_values
@@ -119,6 +119,7 @@ class SpotGridPSF(PSF):
         yy = slice(y_ccd_begin, (y_ccd_begin+ny_ccd))
         
 
+        itt.detach()
         
         return xx,yy,ccd_pix_spot_values
 
