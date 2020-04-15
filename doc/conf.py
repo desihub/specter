8b4cd2b40d86c8a6a85ae40077752acdff9fe80c
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-04-15 13:17:39 -0700

    fix numba import

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,7 @@ napoleon_include_private_with_doc = True
 # some external dependencies are not met at build time and break the
 # building process.
 autodoc_mock_imports = []
-for missing in ('astropy', 'desiutil', 'scipy'):
+for missing in ('astropy', 'desiutil', 'numba', 'scipy'):
     try:
         foo = import_module(missing)
     except ImportError:
